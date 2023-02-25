1afa98e485dc2ff7d6345bb2cef3312c7f187cbf
Ilya Nikolaev <rrypmk3uvoc8@mail.ru>
2023-02-26 01:36:20 +0300

    Added Folder and File types

--- app/db_api/models.py
+++ app/db_api/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger, Text, Boolean, DateTime
+from sqlalchemy import BigInteger, Text, Boolean, DateTime, ForeignKey
 from sqlalchemy import Column
 from sqlalchemy import true
 from sqlalchemy.sql.functions import now
@@ -15,3 +15,22 @@ class User(Base):
     banned = Column(Boolean, nullable=False, server_default=true())
 
     created_at = Column(DateTime, nullable=False, server_default=now())
+
+
+class Folder(Base):
+    __tablename__ = "folders"
+
+    id = Column(BigInteger, primary_key=True)
+
+    name = Column(Text, nullable=False, unique=True)
+
+
+class File(Base):
+    __tablename__ = "files"
+
+    file_id = Column(BigInteger, primary_key=True)
+
+    name = Column(Text, nullable=False)
+    file_size = Column(BigInteger, nullable=False)
+
+    folder = Column(ForeignKey("folders.id"))
